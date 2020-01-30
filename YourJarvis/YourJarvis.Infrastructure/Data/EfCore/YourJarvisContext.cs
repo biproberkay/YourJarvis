9a0fc2d6f57c1329aebd1f454acc4891442aecb4
Şükrü Berkay <studentberkay@outlook.com>
2020-01-31 00:41:59 +0300

    The basics of front-end handled in this section

--- YourJarvis/YourJarvis.Infrastructure/Data/EfCore/YourJarvisContext.cs
+++ YourJarvis/YourJarvis.Infrastructure/Data/EfCore/YourJarvisContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace YourJarvis.Infrastructure.Data.EfCore
 {
-    class YourJarvisContext
+    public class YourJarvisContext: DbContext
     {
+        public YourJarvisContext(DbContextOptions<YourJarvisContext> options): base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
     }
 }
--- YourJarvis/YourJarvis.WebUI/Startup.cs
+++ YourJarvis/YourJarvis.WebUI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using YourJarvis.Infrastructure.Data.EfCore;
 
 namespace YourJarvis.WebUI
 {
@@ -16,6 +17,8 @@ namespace YourJarvis.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<YourJarvisContext>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +29,15 @@ namespace YourJarvis.WebUI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(name:"defaut", 
+                    template:"{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
