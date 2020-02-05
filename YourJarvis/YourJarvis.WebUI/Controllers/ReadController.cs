49f11380e3bb66d8119969cc4071a45aef9c0feb
Şükrü Berkay(bipro) <biproberkay@outlook.com>
2020-02-05 23:10:17 +0300

    ReadController ı yanlış almışım conflict esnasında şimdi tamam oldu bu commiti tagleyebilirim.

--- YourJarvis/YourJarvis.WebUI/Controllers/ReadController.cs
+++ YourJarvis/YourJarvis.WebUI/Controllers/ReadController.cs
@@ -4,32 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using YourJarvis.ApplicationCore.Entities;
+using YourJarvis.ApplicationCore.Entities.ArticleAggregate;
 using YourJarvis.ApplicationCore.ServiceInterfaces;
 
 namespace YourJarvis.WebUI.Controllers
 {
     public class ReadController : Controller
     {
-        private IArticleService _articleService;
-        //private IAlanService _alanService;
+        private IServiceRepository<Alan> _alanService;
+        private IServiceRepository<Article> _articleService;
 
-        public ReadController(
-                                IArticleService articleService
-                                //, IAlanService alanService
-                                )
+        public ReadController(IServiceRepository<Alan> alanService, IServiceRepository<Article> articleService)
         {
+            _alanService = alanService;
             _articleService = articleService;
-            //_alanService = alanService;
         }
-        public IActionResult Index()
+        public IActionResult AlanIndex()
+        {
+            return View(_alanService.GetAll());
+        }
+        public IActionResult ArticleIndex()
         {
             return View(_articleService.GetAll());
-        }
-        public IActionResult AlanIndex() 
-        {
-            return
-                Content("Bu iş olmadı");
-            //View(_alanService.GetAll());
         }
     }
 }
