7dfb5e3ec3af45338f1d97fe972b6db3262a41f3
Şükrü Berkay(bipro) <biproberkay@outlook.com>
2020-02-09 15:07:30 +0300

    Bazı düzenlemeler ve eklemeler

---
+++ YourJarvis/YourJarvis.WebUI/Areas/Admin/Models/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourJarvis.ApplicationCore.Entities;
+using YourJarvis.ApplicationCore.Entities.ArticleAggregate;
+
+namespace YourJarvis.WebUI.Areas.Admin.Models.ViewModels
+{
+    public class ArticleViewModel
+    {
+        public IEnumerable<Article> Articles { get; set; } 
+        public IEnumerable<ArticleTag> ArticleTags { get; set; }
+        public IEnumerable<Alan> Alans { get; set; } 
+    }
+}
---
+++ YourJarvis/YourJarvis.WebUI/ViewComponents/AlanTreeViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourJarvis.ApplicationCore.Entities;
+using YourJarvis.ApplicationCore.ServiceInterfaces;
+
+namespace YourJarvis.WebUI.ViewComponents
+{
+    public class AlanTreeViewComponent : ViewComponent
+    {
+        private IServiceRepository<Alan> _alanService;
+
+        public AlanTreeViewComponent(IServiceRepository<Alan> alanService)
+        {
+            _alanService = alanService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_alanService.GetAll());
+        }
+    }
+}
